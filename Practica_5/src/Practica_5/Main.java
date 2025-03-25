52eb6bce3bc1e561399e5e958973acedc447e17a
DilanM23 <145023346+DilanM23@users.noreply.github.com>
2025-03-25 17:15:07 -0400

    Practica 5

--- Practica_5/src/Practica_5/Main.java
+++ Practica_5/src/Practica_5/Main.java
@@ -5,30 +5,32 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		//Ecuacion Cuadratica
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("EJERCICIO: 1 - ECUACION CUADRATICA");
+		System.out.print("Intro. el valor de a: ");
+		double a = scanner.nextDouble();
+		System.out.print("Intro. el valor de b: ");
+		double b = scanner.nextDouble();
+		System.out.print("Intro. el valor de c: ");
+		double c = scanner.nextDouble();
+		// a!=0
+		if (a == 0) {
+		System.out.println("El valor de 'a' es 0.");
+		} 
+		else {
+			EcuacionCuadratica ecuacion = new EcuacionCuadratica(a, b, c);
+		    ecuacion.mostrarSolucion();
+		}
 		// ESTADISTA
+		System.out.println();
+		System.out.println("EJERCICIO 2- PROMEDIO Y DESVIACION ESTANDAR");
 		Estadistica e1 = new Estadistica();
 		e1.llenar();
 		e1.mostrar();
 		e1.promedio();
 		e1.desviacion();
 		
-		//Ecuacion Cuadratica
-		Scanner scanner = new Scanner(System.in);
 		
-        System.out.print("Intro. el valor de a: ");
-        double a = scanner.nextDouble();
-        System.out.print("Intro. el valor de b: ");
-        double b = scanner.nextDouble();
-        System.out.print("Intro. el valor de c: ");
-        double c = scanner.nextDouble();
-        
-        // a!=0
-        if (a == 0) {
-            System.out.println("El valor de 'a' es 0.");
-        } 
-        else {
-            EcuacionCuadratica ecuacion = new EcuacionCuadratica(a, b, c);
-            ecuacion.mostrarSolucion();
-        }
 	}
 }
